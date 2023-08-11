082b1b240359199e5d8279c402c7f156ad605594
Anisol19 <imanisol19@gmail.com>
2023-08-11 11:49:19 +0530

    Create main.jsx

---
+++ src/main .jsx
@@ -0,0 +1,17 @@
+import ReactDOM from 'react-dom/client'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import AddRestaurant from './pages/AddRestaurant/AddRestaurant'
+import ShowCase from './pages/ShowCase/ShowCase'
+import RestaurantPage from './pages/RestaurantPage/RestaurantPage'
+import User from './pages/User/User'
+import GetTheApp from './pages/GetTheApp/GetTheApp'
+import ErrorPage from './pages/ErrorPage/ErrorPage'
+import SkipedPage from './pages/SkipedPage/SkipedPage'
+import UserSettingsPage from './pages/UserSettingsPage/UserSettingsPage'
+
+import TestPage from './pages/TestPage/TestPage'
+// import AddRestaurantHeader from './components/AddRestaurantHeader/AddRestaurantHeader'
+import App from './App'
+import './index.css'
