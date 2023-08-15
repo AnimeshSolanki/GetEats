ff1043ca7e161416ce9f78467a4ccf2c18bcda79
Animesh Solanki <imanisol19@gmail.com>
2023-08-15 15:33:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,3 +5,13 @@ import PopularPlaces from './components/HomeComponents/PopularPlaces/PopularPlac
 import GetTheApp from './components/HomeComponents/GetTheApp/GetTheApp'
 import ExploreOptionsNearMe from './components/HomeComponents/ExploreOptionsNearMe/ExploreOptionsNearMe'
 import Footer from './components/Footer/Footer'
+
+import orderOnlineImg from '/images/orderonline.jpg'
+import diningoutImg from '/images/diningout.jpg'
+import proandproplusImg from '/images/proandproplus.jpg'
+import nightlifeandclubsImg from '/images/nightlifeandclubs.jpg'
+
+import css from './App.module.css'
+
+import { orderOnlinePage, diningOutPage, proAndProPlusPage, nightLifePage } from './helpers/constants';
+
