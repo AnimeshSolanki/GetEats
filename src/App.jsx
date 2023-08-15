1e097c174ba0e47c47b1c07ee0181e36c298e49b
Animesh Solanki <imanisol19@gmail.com>
2023-08-15 15:34:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,3 +15,22 @@ import css from './App.module.css'
 
 import { orderOnlinePage, diningOutPage, proAndProPlusPage, nightLifePage } from './helpers/constants';
 
+function App() {
+
+    return <>
+      <HomePageBanner />
+      <div className={css.bodySize}>
+        <div className={css.chooseTypeCards}>
+          <SmallCard imgSrc={orderOnlineImg} text="Order Online" link={"/show-case?page=" + orderOnlinePage} />
+          <SmallCard imgSrc={diningoutImg} text="Dining Out" link={'/show-case?page=' + diningOutPage} />
+          <SmallCard imgSrc={proandproplusImg} text="Pro and Pro Plus" link={'/show-case?page=' + proAndProPlusPage} />
+          <SmallCard imgSrc={nightlifeandclubsImg} text="Night Life and Clubs" link={'/show-case?page=' + nightLifePage} />
+        </div>
+        <Collections />
+        <PopularPlaces />
+      </div>
+      <GetTheApp />
+      <ExploreOptionsNearMe />
+      <Footer />
+    </>
+}
