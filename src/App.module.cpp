9d69c7f56c46e5a4e4cfdeb5e2e5eeaa02bbb897
Anisol19 <imanisol19@gmail.com>
2023-08-17 20:19:37 +0530

    Update App.module.cpp

--- src/App.module.cpp
+++ src/App.module.cpp
@@ -2,3 +2,11 @@
     width: 70%;
     margin: auto;
 }
+
+.chooseTypeCards {
+    width: 100%;
+    /* max-height: 500px; */
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
