46cc03b7af4c9e962c6ab9ebb770a69e8c4315f3
Anisol19 <imanisol19@gmail.com>
2023-08-22 15:52:37 +0530

    Update constants.js

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -2,3 +2,9 @@ export const orderOnlinePage = 'order-online';
 export const diningOutPage = 'dinning-out';
 export const proAndProPlusPage = 'pro-and-pro-plus';
 export const nightLifePage = 'night-life';
+
+export const reviewPage = 'reviews'
+export const photosPage = 'photos'
+export const followersPage = 'followers'
+export const recentlyviewedPage = 'recently-viewed'
+export const bookmarksPage = 'bookmarks'
