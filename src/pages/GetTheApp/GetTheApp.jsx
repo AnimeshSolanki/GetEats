a0df24213cce6059250676670ce3aa9fe67752e1
Anisol19 <imanisol19@gmail.com>
2023-10-04 19:58:34 +0530

    Create GetTheApp.jsx

---
+++ src/pages/GetTheApp/GetTheApp.jsx
@@ -0,0 +1,17 @@
+import css from './GetTheApp.module.css'
+
+import Navbar2 from '../../components/Navbars/NavigationBar2/NavigationBar2'
+import GetAppComp from '../../components/HomeComponents/GetTheApp/GetTheApp'
+import Footer from '../../components/Footer/Footer'
+
+const GetApp = () => {
+  return <div className={css.outerDiv}>
+    <div className={css.innerDiv}>
+        <Navbar2 />
+        <GetTheAppComp />
+        <Footer />
+    </div>
+  </div>
+}
+
+export default GetApp
